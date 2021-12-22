b563581d63ff2dae32e824c3e56555dc52dc7ab0
jintae_park <dktkd7044@gmail.com>
2021-12-22 10:38:58 +0900

    init project

---
+++ src/main/java/notice/growingbulletinboard/GrowingBulletinBoardApplication.java
@@ -0,0 +1,13 @@
+package notice.growingbulletinboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowingBulletinBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowingBulletinBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/notice/growingbulletinboard/GrowingBulletinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package notice.growingbulletinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowingBulletinBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
